f317135060adfe2b97f89a4621adccb0a58f31f1
ivros <143576549+ivros@users.noreply.github.com>
2024-10-13 20:55:18 +0000

    Reset build for clean build. run: 'cmake ..' then 'make'

--- build/controllers/footbot_flocking/CMakeFiles/footbot_flocking.dir/footbot_flocking_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-controllers/footbot_flocking/CMakeFiles/footbot_flocking.dir/footbot_flocking_autogen/mocs_compilation.cpp.o: \
- /home/qlu/Documents/argos3-flocking/build/controllers/footbot_flocking/footbot_flocking_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
--- build/loop_functions/flocking_loop_functions/CMakeFiles/flocking_loop_functions_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/flocking_loop_functions_autogen"
-  "flocking_loop_functions_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/flocking_loop_functions_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
